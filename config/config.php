4ca5c802676df0dda4f9dc59d6f9efa4552822e5
kami <tejas@trustshipping.org>
2024-12-05 17:15:57 +0530

    DB Updated

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'login');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_NAME', 'kamilmwg_cv_management');
+define('DB_USER', 'kamilmwg_test');
+define('DB_PASS', 'Test@db24');
